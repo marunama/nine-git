8a01d3b284035e153ac5e378c4a6fa55a799b902
marunama <marunama0120@gmail.com>
2021-10-31 15:49:18 +0900

    デプロイ完了　チャットメッセージはまだ

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const functions = require("firebase-functions");
+//const functions = require("firebase-functions");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -9,39 +9,121 @@ const functions = require("firebase-functions");
 // });
 
 
-exports.sendPushMessage = functions.firestore
-  .collection("chatRooms")
-  .document("{charroomDoId}")
-  .collection("messages")
-  .onWrite(async (change, _context) => {
-    const data = change.after.data();
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+const firestore = admin.firestore();
 
-    // uidから通知先のユーザー情報を取得
-    const userRef = await admin
-      .firestore()
-      .collection("users")
-      .doc(data.uid);
-    const userDoc = await userRef.get();
+// プッシュ通知を送る関数
+const sendPushMessage = function(token, title, body, badge) {
 
-    if (userDoc.exists) {
-      const user = userDoc.data();
-      // 通知のタイトルと本文を設定
-      const payload = {
+    const payload = {
         notification: {
-          title: `（新着）`,
-          body: data.message
+            title: title,
+            body: body,
+            badge: badge,
+            sound:"default"
         }
-      };
+    };
+    const option = {
+        priority: "high"
+    };
+    // ここで実際に通知を送信している
+    console.log("通知")
+    admin.messaging().sendToDevice(token,payload,option);
+}
 
-      /// プッシュ通知を送信
-      if (user.fcmToken) {
-        admin.messaging().sendToDevice(user.fcmToken, payload);
-      } else {
-        console.error("No Firebase Cloud Messaging Token.");
-      }
-    } else {
-      console.error("No User.");
-    }
+// 新規依頼作時
+exports.createMessage = functions.firestore.document('/chatRooms/{chatRooms}')
+    .onWrite( async (snapshot, context) => {
+        // ここにmessageのデータが入っている(senderId,senderName,receiverId,content)
+        const message = snapshot.after.data()
+        console.log(message)
+        const receiverRef = firestore.collection("users") // userにidが付与されていないようなので、一旦emailで誰に渡すのか判別するようにします。
+        console.log("receiverRef")
+        console.log(receiverRef)
+        // 受信者の情報にアクセスする
+        receiverRef.get().then(function(doc){
+                if (doc.exists === true) {
+                    // 受信者の情報を取得(name,fcmToken)
+                    const receiver = doc.data()
+                    const fcmToken = receiver["fcmToken"]
+                    const senderName = message["username"]
+                    const content = message["content"]
+                    // 通知のタイトル
+                    const title = `$senderName`
+                    // 通知の内容
+                    const body = `$content`
+                    sendPushMessage(fcmToken,title,body,"1");
+                    console.log("newMessage")
+                 } else {
+                    console.log("notExists")
+                }
+            })
+})
 
-    return true;
-  });
+
+//exports.updateMessage = functions.firestore.document('/chatRooms/wlV45KGLEDRElBAI4IFH/')
+//    .onUpdate( async (snapshot, context) => {
+//        // ここにmessageのデータが入っている(senderId,senderName,receiverId,content)
+//        const message = snapshot.data()
+//        console.log(message)
+//        const receiverRef = firestore.collection("users").doc(message["email"]) // userにidが付与されていないようなので、一旦emailで誰に渡すのか判別するようにします。
+//        console.log("receiverRef")
+//        console.log(receiverRef)
+//        // 受信者の情報にアクセスする
+//        receiverRef.get().then(function(doc){
+//                if (doc.exists === true) {
+//                    // 受信者の情報を取得(name,fcmToken)
+//                    const receiver = doc.data()
+//                    const fcmToken = receiver["fcmToken"]
+//                    const senderName = message["username"]
+//                    const content = message["content"]
+//                    // 通知のタイトル
+//                    const title = `$senderName`
+//                    // 通知の内容
+//                    const body = `$content`
+//                    sendPushMessage(fcmToken,title,body,"1");
+//                    console.log("newMessage")
+//                 } else {
+//                    console.log("notExists")
+//                }
+//            })
+//})
+
+// exports.sendPushMessage = firestore
+//   .collection("chatRooms")
+//   .document("{chatRoomsDoId}")
+//   .collection("messages")
+//   .onWrite(async (change, _context) => {
+//     const data = change.after.data();
+
+//     // uidから通知先のユーザー情報を取得
+//     const userRef = await admin
+//       .firestore()
+//       .collection("users")
+//       .doc(data.uid);
+//     const userDoc = await userRef.get();
+
+//     if (userDoc.exists) {
+//       const user = userDoc.data();
+//       // 通知のタイトルと本文を設定
+//       const payload = {
+//         notification: {
+//           title: `（好きなタイトルを入力）`,
+//           body: data.message
+//         }
+//       };
+
+//       /// プッシュ通知を送信
+//       if (user.fcmToken) {
+//         admin.messaging().sendToDevice(user.fcmToken, payload);
+//       } else {
+//         console.error("No Firebase Cloud Messaging Token.");
+//       }
+//     } else {
+//       console.error("No User.");
+//     }
+
+//     return true;
+//   });
